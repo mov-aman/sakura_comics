fe98a595504a4b8c88de020ae75dccdc46481598
Aman Singh <aman.12101913@lpu.in>
2024-02-02 05:14:58 +0530

    home page

---
+++ client/src/Components/Assets/data.js
@@ -0,0 +1,38 @@
+import p1_img from './product_1.jpeg';
+import p2_img from './product_2.jpeg';
+import p3_img from './product_3.jpg';
+import p4_img from './product_4.jpeg';
+
+let data_product = [
+    {
+      id: 1,
+      name: "Demon Slayer",
+      image: p1_img,
+      new_price: 199.00,
+      old_price: 249.00,
+    },
+    {
+      id: 2,
+      name: "My Hero Academia",
+      image: p2_img,
+      new_price: 239.00,
+      old_price: 299.00,
+    },
+    {
+      id: 3,
+      name: "Attack on Titan",
+      image: p3_img,
+      new_price: 299.00,
+      old_price: 349.00,
+    },
+    {
+      id: 4,
+      name: "One Punch Man",
+      image: p4_img,
+      new_price: 229.00,
+      old_price: 299.00,
+    },
+  ];
+  
+  export default data_product;
+  
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Hero from '../Components/Hero/Hero';
+import Popular from '../Components/Popular/Popular';
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Hero/>
+      <Popular/>
+    </div>
   )
 }
 
