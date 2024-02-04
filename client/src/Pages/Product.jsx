60b145403a63d307ebb35f93b0ed602128a845b7
Aman Singh <aman.12101913@lpu.in>
2024-02-05 02:55:53 +0530

    add login page and some other pages

--- client/src/Pages/Product.jsx
+++ client/src/Pages/Product.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { EcomContext } from '../Context/EcomContext';
+import {useParams} from 'react-router-dom';
+import Breadcrum from '../Components/BreadCrum/Breadcrum';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 
-function Product() {
+const Product = () => {
+  const {all_product} = useContext(EcomContext);
+  const {productId} = useParams();
+  const product = all_product.find((e)=> e.id ===Number(productId));
   return (
-    <div>Product</div>
+    <div>
+      <Breadcrum product = {product} />
+      <ProductDisplay product = {product}/>
+    </div>
   )
 }
 
