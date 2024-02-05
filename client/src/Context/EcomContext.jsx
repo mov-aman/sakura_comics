e7f4b40bf2857647306e6cf8db45ed5b6b63af69
Aman Singh <aman.12101913@lpu.in>
2024-02-05 07:50:49 +0530

    cart icon is working

--- client/src/Context/EcomContext.jsx
+++ client/src/Context/EcomContext.jsx
@@ -44,7 +44,17 @@ const EcomContextProvider = (props) => {
         }
         return totalAmount;
     }
-    const contextValue = { getTotalCartAmount, all_product, cartItems, addToCart, removeFromCart };
+
+    const getTotalCartItems = () => {
+        let totalItems = 0;
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                totalItems += cartItems[item]
+            }
+        }
+        return totalItems;
+    }
+    const contextValue = { getTotalCartItems, getTotalCartAmount, all_product, cartItems, addToCart, removeFromCart };
     return (
         <EcomContext.Provider value={contextValue}>
             {props.children}
