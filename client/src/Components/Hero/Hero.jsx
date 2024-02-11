68764d9f3b48e6ab7889a8eaea4e0897e6b7dd2e
Aman Singh <aman.12101913@lpu.in>
2024-02-12 02:42:18 +0530

    Responsive hero and items comp

--- client/src/Components/Hero/Hero.jsx
+++ client/src/Components/Hero/Hero.jsx
@@ -7,23 +7,24 @@ import hero_image from '../Assets/hero_1.png'
 export const Hero = () => {
   return (
     <div className='hero'>
-        <div className="hero-left">
-            <h2>NEW ARRIVALS ONLY</h2>
-            <div>
-                <div className="hero-eye-icon">
-                    <p>new</p>
-                    <img src={eye_icon} alt=''/>
-                </div>
-                <p>Collections</p>
-                <p>for everyone</p>
-            </div>
-            <div className="hero-latest-btn">
-              <div>Latest Collection</div>
-              {/* <img src={arrow_icon} alt=''/> */}
-            </div>
+      <div className="hero-left">
+        <h2>NEW ARRIVALS ONLY</h2>
+        <div>
+          <div className="hero-eye-icon">
+            <p>new</p>
+            <img src={eye_icon} alt='' />
+          </div>
+          <p>Collections</p>
+          <p>for everyone</p>
         </div>
-        <div className="hero-right"></div>
-        <img src={hero_image} alt=''/>
+        <div className="hero-latest-btn">
+          <div>Latest Collection</div>
+          {/* <img src={arrow_icon} alt=''/> */}
+        </div>
+      </div>
+      <div className="hero-right">
+        <img src={hero_image} alt='' />
+      </div>
     </div>
   )
 }
