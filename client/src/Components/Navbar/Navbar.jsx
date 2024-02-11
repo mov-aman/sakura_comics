80c608dac98c51715f51446ad57f9a44adb487d9
Aman Singh <aman.12101913@lpu.in>
2024-02-11 20:46:28 +0530

    Navbar is responsive

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,22 +1,30 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import './Navbar.css'
 
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/grocery-store.png'
 import { Link } from 'react-router-dom'
 import { EcomContext } from '../../Context/EcomContext'
+import drop_icon from '../Assets/drop_icon.png';
 
 const Navbar = () => {
 
     const [menu, setMenu] = useState("home");
     const {getTotalCartItems} = useContext(EcomContext);
+    const menuRef = useRef();
+
+    const dropdown_toggle = (e) =>{
+        menuRef.current.classList.toggle('nav-menu-visible');
+        e.target.classList.toggle('open');
+    }
     return (
         <div className='navbar'>
             <div className='nav-logo'>
                 <img src={logo} alt="" />
                 <p>Sakura Comics</p>
             </div>
-            <ul className='nav-menu'>
+            <img className='nav-dropdown' onClick={dropdown_toggle} src={drop_icon} alt="" />
+            <ul ref={menuRef}className='nav-menu'>
                 <li onClick={() => { setMenu("home") }}><Link style={{ textDecoration: 'none' }} to='/'>Home</Link>{menu === "home" ? <hr /> : <></>}</li>
                 <li onClick={() => { setMenu("newReleases") }}><Link style={{ textDecoration: 'none' }} to='/newReleases'>New Release</Link>{menu === "newReleases" ? <hr /> : <></>}</li>
                 <li onClick={() => { setMenu("bestSellers") }}><Link style={{ textDecoration: 'none' }} to='/bestSellers'>Best Seller</Link>{menu === "bestSellers" ? <hr /> : <></>}</li>
